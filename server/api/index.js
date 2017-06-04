e85601c35e66b088c40a2809584ede46c7f9ead5
Moudicat <i@0u0b.com>
2017-06-04 22:16:30 +0800

    [BE] Add route.

--- server/api/index.js
+++ server/api/index.js
@@ -4,11 +4,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/bangumi', require('./bangumi'));
-router.use('/user', require('./user'));
-router.use('/article', require('./article'));
 router.use('/hitokoto', require('./hitokoto'));
+router.use('/article', require('./article'));
 router.use('/status', require('./status'));
+router.use('/user', require('./user'));
+router.use('/bangumi', require('./bangumi'));
+router.use('/animation', require('./animation'));
 router.use('/weather', require('./weather'));
 
 module.exports = router;
