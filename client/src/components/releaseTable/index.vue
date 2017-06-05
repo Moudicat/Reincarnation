1a23d2dd0b0f77051c72840037d1db8d6f8c11dc
Moudicat <i@0u0b.com>
2017-06-06 00:07:52 +0800

    [MIXED] 06061 Build.

--- client/src/components/releaseTable/index.vue
+++ client/src/components/releaseTable/index.vue
@@ -12,9 +12,9 @@
       return {
         releaseData: [
           {
-            version: '0.1.2',
+            version: '0.1.3',
             date: '2017-06-05',
-            desc: '添加 番剧列表接入api 番剧查询添加'
+            desc: '添加 番剧列表接入api 番剧查询添加 补全 所有看过的番 修复 若干bug'
           },
           {
             version: '0.1.0',
