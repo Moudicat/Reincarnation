7ce5778fcd6bd25b1f75ca4d15f7f7a8c1b067c8
Moudicat <i@0u0b.com>
2017-06-08 01:08:33 +0800

    [BE] Update animation to status.

--- server/api/status.js
+++ server/api/status.js
@@ -4,6 +4,7 @@
 const express = require('express');
 const router = express.Router();
 const Status = require('../controllers/status');
+const Animation = require('../controllers/animation');
 const authorization = require('../middleware/authorization');
 
 let resData;
@@ -16,21 +17,35 @@ router.use((req, res, next) => {
 });
 
 router.get('/', (req, res) => {
-  Status.get().then(response => {
-    if (response.length === 0) {
-      resData.msg = '无数据';
-      resData.code = -404;
-      res.json(resData);
-    } else {
-      resData.data = response[0];
-      res.json(resData);
-    }
-  }).catch(err => {
-    console.error(err);
-    resData.msg = err;
-    resData.code = -500;
-    res.json(resData);
-  });
+  Status.get()
+    .then(response => {
+      if (response.length === 0) {
+        resData.msg = '无数据';
+        resData.code = -404;
+        res.json(resData);
+      } else {
+        resData.data = response[0];
+        Animation.getLatest()
+          .then(response => {
+            let animationObj = {
+              name: '最近看的番',
+              content: response.data.name,
+              num: response.data.episode
+            };
+            resData.data.push(animationObj);
+            res.json(resData);
+          })
+          .catch(err => {
+            console.error(err);
+            res.sendStatus(500);
+            return;
+          });
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 });
 
 // 以下的路由需要认证
