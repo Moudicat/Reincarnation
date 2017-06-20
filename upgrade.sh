1b2cc78287e1664a6268e46cfd09dafacb778cfc
Moudicat <i@0u0b.com>
2017-06-20 19:39:01 +0800

    [OTHRE] Add color.

--- upgrade.sh
+++ upgrade.sh
@@ -1,9 +1,9 @@
-echo '-------------Reincarnation 升级程序-------------'
+echo '\033[41;36m -------------Reincarnation 升级程序------------- \033[0m'
 echo '\\n'
-echo 'Step1: 拉取代码'
+echo '\033[42;30m Step1: 拉取代码 \033[0m'
 git pull
-echo '拉取完成'
+echo '\033[41;36m 拉取完成 \033[0m'
 echo '\\n'
-echo 'Step2: 重启服务'
+echo '\033[42;30m Step2: 重启服务 \033[0m'
 pm2 restart 1
-echo '-------------Reincarnation 升级完成-------------'
+echo '\033[41;36m -------------Reincarnation 升级完成------------- \033[0m'
