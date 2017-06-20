8336b8a17476db3c50f348c811f3fdce83386440
Moudicat <i@0u0b.com>
2017-06-20 19:52:52 +0800

    [OTHRE] Add -e.

--- upgrade.sh
+++ upgrade.sh
@@ -1,9 +1,9 @@
-echo '\033[41;36m -------------Reincarnation 升级程序------------- \033[0m'
-echo '\\n'
-echo '\033[42;30m Step1: 拉取代码 \033[0m'
+echo -e '\033[41;36m -------------Reincarnation 升级程序------------- \033[0m'
+echo -e '\\n'
+echo -e '\033[42;30m Step1: 拉取代码 \033[0m'
 git pull
-echo '\033[41;36m 拉取完成 \033[0m'
-echo '\\n'
-echo '\033[42;30m Step2: 重启服务 \033[0m'
+echo -e '\033[41;36m 拉取完成 \033[0m'
+echo -e '\\n'
+echo -e '\033[42;30m Step2: 重启服务 \033[0m'
 pm2 restart 1
-echo '\033[41;36m -------------Reincarnation 升级完成------------- \033[0m'
+echo -e '\033[41;36m -------------Reincarnation 升级完成------------- \033[0m'
