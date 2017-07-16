af471d8768cbdf52f91c386e8b9699e608508e5a
Moudicat <i@0u0b.com>
2017-07-17 01:27:10 +0800

    [FE] Improve Best Practices.

--- client/src/views/footer/index.vue
+++ client/src/views/footer/index.vue
@@ -1,6 +1,6 @@
 <template>
   <footer>
-    <p>Powered By <a href="https://github.com/moudicat/reincarnation">Reincatnation@Beta</a> | <a href="http://www.miitbeian.gov.cn/"  target="_blank">辽ICP备15016208号-1</a></p>
+    <p>Powered By <a href="https://github.com/moudicat/reincarnation">Reincatnation@Beta</a> | <a href="http://www.miitbeian.gov.cn/"  target="_blank" rel="nofollow noopener noreferrer">辽ICP备15016208号-1</a></p>
     <p>Copyright © 2014-2017 某迪猫 All Rights Reserved.</p>
   </footer>
 </template>
