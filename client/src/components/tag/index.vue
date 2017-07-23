57f829e3119c4a6bc3d30fbdb416ec5a0a97c33d
Moudicat <i@0u0b.com>
2017-07-23 16:53:28 +0800

    [FE] Impove tag component.

--- client/src/components/tag/index.vue
+++ client/src/components/tag/index.vue
@@ -41,25 +41,40 @@
     background-color: #e4e8f1;
     border-color: #e4e8f1;
     color: #48576a;
+    > * {
+      color: #48576a;
+    }
   }
   .blue {
     background-color: rgba(32,160,255,.1);
     border-color: rgba(32,160,255,.2);
     color: #20a0ff;
+    > * {
+      color: #20a0ff;
+    }
   }
   .green {
     background-color: rgba(18,206,102,.1);
     border-color: rgba(18,206,102,.2);
     color: #13ce66;
+    > * {
+      color: #13ce66;
+    }
   }
   .orange {
     background-color: rgba(247,186,41,.1);
     border-color: rgba(247,186,41,.2);
     color: #f7ba2a;
+    > * {
+      color: #f7ba2a;
+    }
   }
   .red {
     background-color: rgba(255,73,73,.1);
     border-color: rgba(255,73,73,.2);
     color: #ff4949;
+    > * {
+      color: #ff4949;
+    }
   }
 </style>
