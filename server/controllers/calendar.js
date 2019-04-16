b7b597a5bc08d317a0f11b8976537fcd07691368
Moudicat <i@0u0b.com>
2019-04-16 17:17:49 +0800

    A 新增程序员老黄历

---
+++ server/controllers/calendar.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Moudi on 2017/5/26.
+ */
+
+import BaseRouterController from './base/baseRouterController';
+import { controller, get } from '../decorators/router';
+import rplMiddleware from '../middlewares/rplMiddleware';
+import calendar from '../services/programmerCalendar';
+
+@controller('/calendar', rplMiddleware)
+export default class Weather extends BaseRouterController {
+  @get('/')
+  get(req, res) {
+    console.log(calendar.getTodayString());
+    try {
+      res.resData.data = {
+        today: calendar.getTodayString(),
+        direction: calendar.getDirectionString(),
+        lucky: calendar.pickTodaysLuck()
+      };
+      res.json(res.resData);
+    } catch (err) {
+      console.error(err);
+      res.sendStatus(500);
+    }
+  }
+}
