58fa33a87813521489580aa9571e5a56530360e3
Moudicat <i@0u0b.com>
2019-06-27 11:12:12 +0800

    Build 修复前端页面baseURL及备案号

--- client/src/views/footer/index.vue
+++ client/src/views/footer/index.vue
@@ -1,25 +1,25 @@
 <template>
   <footer>
-    <p>Powered By <a href="https://github.com/moudicat/reincarnation">Reincarnation@Beta</a> | <a href="http://www.miitbeian.gov.cn/"  target="_blank" rel="nofollow noopener noreferrer">辽ICP备15016208号-1</a></p>
+    <p>Powered By <a href="https://github.com/moudicat/reincarnation">Reincarnation@Beta</a> | <a href="http://www.miitbeian.gov.cn/"  target="_blank" rel="nofollow noopener noreferrer">黔ICP备19005521号-1</a></p>
     <p>Copyright © 2014-{{ new Date().getFullYear() }} 某迪猫 All Rights Reserved.</p>
   </footer>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {};
+export default {};
 </script>
 
 <style lang="scss" rel="stylesheet/scss">
-  footer {
-    flex: 0 0 170px;
-    padding: 40px;
-    text-align: center;
-    p {
-      font-size: 14px;
-      padding: 5px 0;
-      a {
-        color: $primary-text;
-      }
+footer {
+  flex: 0 0 170px;
+  padding: 40px;
+  text-align: center;
+  p {
+    font-size: 14px;
+    padding: 5px 0;
+    a {
+      color: $primary-text;
     }
   }
+}
 </style>
