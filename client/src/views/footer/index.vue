32e44fb83d9eaa465ec89a3c1e86cf1b4d6e5c28
Moudicat <i@0u0b.com>
2020-03-11 18:46:16 +0800

    Fix 修复底部备案

--- client/src/views/footer/index.vue
+++ client/src/views/footer/index.vue
@@ -1,6 +1,6 @@
 <template>
   <footer>
-    <p>Powered By <a href="https://github.com/moudicat/reincarnation">Reincarnation@Beta</a> | <a href="http://www.miitbeian.gov.cn/"  target="_blank" rel="nofollow noopener noreferrer">黔ICP备19005521号-1</a></p>
+    <p>Powered By <a href="https://github.com/moudicat/reincarnation">Reincarnation@Beta</a> | <a href="http://www.beian.miit.gov.cn/"  target="_blank" rel="nofollow noopener noreferrer">黔ICP备19005521号-1</a></p>
     <p>Copyright © 2014-{{ new Date().getFullYear() }} 某迪猫 All Rights Reserved.</p>
   </footer>
 </template>
